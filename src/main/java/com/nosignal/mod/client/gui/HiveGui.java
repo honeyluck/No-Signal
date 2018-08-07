8051e901c5b53e15452d7085ccc8f1382b3b8e77
LotuxPunk <clement@vandendaelen.com>
2018-08-07 16:28:17 +0200

    Bees back !

---
+++ src/main/java/com/nosignal/mod/client/gui/HiveGui.java
@@ -0,0 +1,16 @@
+package com.nosignal.mod.client.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class HiveGui extends GuiContainer {
+
+    public HiveGui(Container inventorySlotsIn) {
+        super(inventorySlotsIn);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+
+    }
+}
