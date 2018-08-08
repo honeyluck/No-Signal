3a75efb22a963852befc9123b4ffd7473c8beeda
LotuxPunk <clement@vandendaelen.com>
2018-08-08 19:57:17 +0200

    Hive generation working !

--- src/main/java/com/nosignal/mod/world/gen/HiveGen.java
+++ src/main/java/com/nosignal/mod/world/gen/HiveGen.java
@@ -1,42 +1,74 @@
 package com.nosignal.mod.world.gen;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.block.state.pattern.BlockMatcher;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraft.world.gen.feature.WorldGenBlockBlob;
-import net.minecraft.world.gen.feature.WorldGenMinable;
-import net.minecraft.world.gen.feature.WorldGenTrees;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
-import java.util.function.Predicate;
 
 import static com.nosignal.mod.events.Registries.BLOCK_HIVE;
 
 public class HiveGen implements IWorldGenerator {
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-        if (world.provider.getDimension() == 0){
-            runGenerator(BLOCK_HIVE.getDefaultState(),1,60,64,256, BlockMatcher.forBlock(Blocks.LEAVES),world, random,chunkX,chunkZ);
+
+        final Biome biome = world.getBiomeForCoordsBody(new BlockPos(chunkX, 0, chunkZ));
+
+        if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.END) || BiomeDictionary.hasType(biome, BiomeDictionary.Type.NETHER)) {
+            return;
         }
+
+        tryGenerateHive(world, random,chunkX,chunkZ);
     }
 
-    private void runGenerator (IBlockState blockToGen, int blockAmount, int chancesToSpawn, int minHeight, int maxHeight, Predicate<IBlockState> blockToReplace, World world, Random rand, int chunk_X, int chunk_Z){
-        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
-            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
-        WorldGenMinable generator = new WorldGenMinable(blockToGen,blockAmount,(com.google.common.base.Predicate<IBlockState>) blockToReplace);
-        int heightdiff = maxHeight - minHeight +1;
+    private static void  tryGenerateHive (World world, Random rand, int chunk_X, int chunk_Z){
+        final BlockPos.MutableBlockPos variableBlockPos = new BlockPos.MutableBlockPos();
 
-        for (int i=0; i<chancesToSpawn; i++){
-            int x = chunk_X * 16 +rand.nextInt(16);
-            int y = minHeight + rand.nextInt(heightdiff);
-            int z = chunk_Z * 16 + rand.nextInt(16);
+        int x = chunk_X + rand.nextInt(16);
+        int z = chunk_Z + rand.nextInt(16);
+        int y = world.getHeight(x, z) - 1;
+        variableBlockPos.setPos(x, y, z);
 
-            generator.generate(world, rand, new BlockPos(x, y, z));
-        }
+        if (!isBlockLeaves(world, variableBlockPos)) return;
+
+        int newY = getHeightBelowLeaves(world, x, y, z);
+
+        if (newY < 0) return;
+
+        variableBlockPos.setY(newY);
+        //System.out.println(variableBlockPos.toString());
+        world.setBlockState(variableBlockPos, BLOCK_HIVE.getDefaultState());
     }
+
+    private static boolean isBlockLeaves(World world, BlockPos blockPos) {
+        IBlockState blockState = world.getBlockState(blockPos);
+        final Block block = blockState.getBlock();
+        return block.isLeaves(blockState, world, blockPos);
+    }
+
+    private static int getHeightBelowLeaves(World world,int posX, int posY, int posZ){
+        final BlockPos.MutableBlockPos variableBlockPos = new BlockPos.MutableBlockPos();
+        variableBlockPos.setPos(posX, posY, posZ);
+
+        //Try to get a air position below a leave. If not, return -1
+        for (int y = posY, sealevel = world.getSeaLevel(); y >= sealevel; --y) {
+            variableBlockPos.setY(y);
+            final IBlockState blockState = world.getBlockState(variableBlockPos);
+            final Block block = blockState.getBlock();
+
+            if (block.isLeaves(blockState, world, variableBlockPos)) continue;
+
+            if (world.isAirBlock(variableBlockPos)) return y;
+
+            return -1;
+        }
+        return -1;
+    }
+
 }
